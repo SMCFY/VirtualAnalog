7aee8a34d6c5854c113b83fdc3c933ace9768c5c
NikolajAndersson <nikolajandersson@gmail.com>
2017-03-03 13:11:58 +0100

    phasor with comments

--- phasor.m
+++ phasor.m
@@ -1,21 +1,19 @@
 % Analog phasor described in DAFX
-% R < 1 is the radius of each pole in a complex-conjugate pole pair, and the pole angles are ±?. 
-% The pole angle ? ? (0, ? ) can be interpreted as ? = ?n T , 
-% where ?n is the desired notch frequency and T is the sampling interval. 
+% R < 1 is the radius of each pole in a complex-conjugate pole pair, and the pole angles are ±theta. 
+% The pole angle theta (0, pi ) can be interpreted as theta = omega*T , 
+% where omega is the desired notch frequency and T is the sampling interval. 
 % The pole radius R controls the width of the notch ? the closer it is to 1, 
-% the narrower the notch (and the more accurate is the tuning parameter ?).
+% the narrower the notch (and the more accurate is the tuning parameter theta).
 
 
-fs = 44100; %fs/2
-
+fs = 44100; 
 [x,fs] = audioread('highpitchchords.wav');
 x = x(:,1);
 t = 44100*2; % length of sound in samples
+
+% noise
 %x = 2*rand(1,t);
 %x = x - mean(x); % remove DC off
-%pluck = linspace(0,N);
-%x = (pluck);
-%x = [1, zeros(1,N-1)]; % Impulse response
 
 out = 0;
 signal = 0;
@@ -25,8 +23,8 @@ if t > length(x)
     x = [x zeros(1,diff)];
 end
 
-R1 = 0.99;R2 = 0.99;R3 = 0.99;R4 = 0.8;
-theta1 = 440*2*pi;theta2 = 500*2*pi;theta3 = 800*2*pi;theta4 = 100*2*pi;
+R1 = 0.5;R2 = 0.9;R3 = 0.2;R4 = 0.8;
+theta1 = 440*2*pi; theta2 = 500*2*pi; theta3 = 800*2*pi; theta4 = 100*2*pi;
 
 a11 = -2*R1*cos(theta1);
 a21 = R1^2;
@@ -52,22 +50,26 @@ wet = 0.6;
 dry = 1-wet;
 
 for i = 1:t % t is the iterations
-    theta1 = 440*pi*(i)/fs;
+    theta1 = 440*2*pi*(i)/fs;
     if theta1 > pi
         theta1 = theta1 - pi;
     end
-    theta2 = 240*pi*(i)/fs;
+    
+    theta2 = 240*2*pi*(i)/fs;
     if theta2 > pi
         theta2 = theta2 - pi;
     end
-    theta3 = 140*pi*(i)/fs;
+    
+    theta3 = 140*2*pi*(i)/fs;
     if theta3 > pi
         theta3 = theta3 - pi;
     end
-    theta4 = 840*pi*(i)/fs;
+    
+    theta4 = 840*2*pi*(i)/fs;
     if theta4 > pi
         theta4 = theta4 - pi;
     end
+    
     a11 = -2*R1*cos(theta1);
 
     a12 = -2*R2*cos(theta2);
@@ -108,6 +110,7 @@ for i = 1:t % t is the iterations
     AP4(3) = AP4(2);
     AP4(2) = AP4(1);
     
+    % mix dry signal x(i) with allpass filtered signal AP4(1)
     signal = [signal, dry*x(i)+wet*AP4(1)];
 end
 plot(signal)
