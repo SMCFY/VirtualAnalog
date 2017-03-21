ec5196f72c99009a14ff54ad2cf9a94c252ab95a
Nikolaj Andersson <nikolajandersson@gmail.com>
2017-03-21 15:15:31 +0100

    Added diode eq and values, jacobian is missing

--- diode_test_model.m
+++ diode_test_model.m
@@ -1,24 +1,25 @@
 function [i, J] = diode_test_model(v)
-% source: Kristjan Dempwolf and Udo Zölzer, A PHYSICALLY-MOTIVATED TRIODE MODEL FOR CIRCUIT SIMULATIONS
-    Gg = 606e-6;
-    xi = 1.354;
-    Cg = 13.9;
-    vgk = v(1); 
-    vpk = v(2);
-    expg = exp(Cg*vgk);
-    ig = -Gg * (log(1+expg)/Cg)^xi;
-    dig_dvgk = -Gg * xi * (log(1+expg)/Cg)^(xi-1) * expg/(1+expg);
-    dig_dvpk = 0;
-
-    Gp = 2.14e-3;
-    gamma = 1.303;
-    Cp = 3.04;
-    mu = 100.8;
-    expp = exp(Cp*(rand(1)/mu+vgk));
-    ip = -Gp * (log(1+expp)/Cp)^gamma - ig;
-    dip_dvgk = -Gp * gamma * (log(1+expp)/Cp)^(gamma-1) * expp/(1+expp);
-    dip_dvpk = dip_dvgk/mu;
+% Model of single nonlinear diode 
+    % values are from master thesis sd-1 
+    % Reverse saturation current
+    Is = 4e-9;
+    % emission coefficient
+    n = 1.65;
+    % voltage at diode's terminal
+    vd = v(1);
+    % Therminal voltage
+    vt = 50e-3;
+    % single diode characteristic voltage out
     
-    i = [ig; ip];
-    J = [dig_dvgk, dig_dvpk; dip_dvgk, dip_dvpk];
+    id = -Is * (exp((vd/n*vt) - 1));
+    % calculate jacobian
+    
+    i = id;
+    J = -Is * (1 / 2 / vt*exp(vd/n*vt)-1);
+    
+    %Anti-parallel Diodes? current
+%ID4561 = -Is * (exp(vd/(2*vt)) - exp(-vd/(vt)));
+%Anti-parallel Diodes? jacobian
+%dID4561dVD4561= -Is * (1/2/vt*exp(vd/(2*vt)) + 1/vt*exp(-vd/(vt)));
+  
 end
