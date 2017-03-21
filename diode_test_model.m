c1cfd89402cdea2a58b3a7a5c1e835e2fe1fb0d9
Nikolaj Andersson <nikolajandersson@gmail.com>
2017-03-21 16:04:29 +0100

    added anti-parallel diode

--- diode_test_model.m
+++ diode_test_model.m
@@ -1,6 +1,6 @@
 function [i, J] = diode_test_model(v)
-% Model of single nonlinear diode 
-    % values are from master thesis sd-1 
+% Model of single nonlinear diode
+    % values are from master thesis sd-1
     % Reverse saturation current
     Is = 4e-9;
     % emission coefficient
@@ -10,16 +10,13 @@ function [i, J] = diode_test_model(v)
     % Therminal voltage
     vt = 50e-3;
     % single diode characteristic voltage out
-    
-    id = -Is * (exp((vd/n*vt) - 1));
+    %id = -Is * (exp((vd/n*vt) - 1));
     % calculate jacobian
-    
-    i = id;
-    J = -Is * (1 / 2 / vt*exp(vd/n*vt)-1);
-    
+    %J = -Is * (1 / 2 / vt*exp(vd/n*vt)-1);
+
     %Anti-parallel Diodes? current
-%ID4561 = -Is * (exp(vd/(2*vt)) - exp(-vd/(vt)));
-%Anti-parallel Diodes? jacobian
-%dID4561dVD4561= -Is * (1/2/vt*exp(vd/(2*vt)) + 1/vt*exp(-vd/(vt)));
-  
+    id = -Is * (exp(vd/(2*vt)) - exp(-vd/(vt)));
+    %Anti-parallel Diodes? jacobian
+    J = -Is * (1/2/vt*exp(vd/(2*vt)) + 1/vt*exp(-vd/(vt)));
+    i = id;
 end
