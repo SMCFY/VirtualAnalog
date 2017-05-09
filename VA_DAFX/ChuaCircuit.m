5743213f31336414f58469c748e0d6c7d1261f5f
Nikolaj Andersson <nikolajandersson@gmail.com>
2017-05-09 15:57:21 +0200

    First attempt at Chua, not working

---
+++ VA_DAFX/ChuaCircuit.m
@@ -0,0 +1,28 @@
+
+% C4 and L in parallel --> Series(P, OpenCircuit) --> Parallel(S, C2) --> Nonlinear Resistor
+fs = 44100;
+len = 20000; % length in samples
+C4val = 49e-5;
+C4 = Capacitor(1/(2*C4val*fs));
+Lval = 7.07e-2;
+L1 = Inductor(1/(2*Lval*fs));
+P1 = Parallel(C4,L1);
+R1 = Resistor(1428); % 1.428kOhm
+S1 = Series(P1, R1);
+C1val = 5.5e-3;
+C1 = Capacitor(1/(2*C1val*fs));
+P2 = Parallel(S1, C1);
+NLR = NLResistor(569.2);
+output = zeros(len,1);
+C1.State = 100; 
+C4.State = 100; 
+L1.State = 100; 
+for i=1:len
+    WaveUp(P2); % get the waves up to the root
+    setWD(NLR, P2.WU);
+    setWD(P2,NLR.WD); 
+    output(n) = Voltage(R1);
+
+end
+%%
+plot(output)
--- VA_DAFX/WDFClasses/NLResistor.m
+++ VA_DAFX/WDFClasses/NLResistor.m
@@ -1,12 +1,32 @@
 %----------------------Non linear Resistor Class------------------------
 classdef NLResistor < OnePort % a (nonlinear) WDF resistor
+    properties
+        G1 = -0.0005; % -500uS
+        G2 = -0.0008; % -800uS
+        v0 = 1;       % 1V
+        %R = 569.2;    % Resistance, can maybe work as PortRes;
+        g1 = 0; 
+        g2 = 0; 
+        a0 = 0;
+        
+    end
     methods
         function obj = NLResistor(PortRes) % constructor function
             obj.PortRes = PortRes; % port resistance (equal to el. res.)
+            R = PortRes;
+            obj.g1 = (1-obj.G1*R)/(1+obj.G1*R);
+            obj.g2 = (1-obj.G2*R)/(1+obj.G2*R);
+            obj.a0 = obj.v0 * (1+obj.G2*R);
         end
         function WU = WaveUp(obj) % get the up-going wave
+            
             WU = 0; % always zero for a linear WDF resistor
             obj.WU = WU;
+        end
+        function setWD(obj, a) % get the down-going wave
+            
+            WD = obj.g1*a+1/2*(obj.g2-obj.g1)*(abs(a + obj.a0) - abs(a - obj.a0)); % eq. 17 DIGITAL SIMULATION OF NONLINEAR CIRCUITS BY WAVE DIGITAL FILTER PRINCIPLES, Meerkötter and Scholz
+            obj.WD = WD;
         end
     end
 end
