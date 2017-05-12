1a4566b6d28ba2cc4f44c151824196b912041cad
Nikolaj Andersson <nikolajandersson@gmail.com>
2017-05-12 16:06:25 +0200

    2 port parallel

---
+++ VA_DAFX/WDFClasses/TwoPortParallel.m
@@ -0,0 +1,56 @@
+%----------------------Parallel Switch Class------------------------
+classdef TwoPortParallel < Adaptor % the class for parallel 3-port adaptors
+    properties
+        WD = 0;% this is the down-going wave at the adapted port
+        WU = 0;% this is the up-going wave at the adapted port
+    end
+    methods
+        function obj = Parallel(KidLeft, KidRight) % constructor function
+            obj.KidLeft = KidLeft; % connect the left 'child'
+            obj.KidRight = KidRight; % connect the right 'child'
+           
+            obj.PortRes = (KidLeft.PortRes - KidRight.PortRes)/(KidLeft.PortRes + KidRight.PortRes);% obj.G2+obj.G3; % parallel adapt. port facing the root
+        end
+        function WU = WaveUp(obj) % the up-going wave at the adapted port
+            % where to send the upgoing wave? 
+        end
+        function setWD(obj,WaveFromParent) %  sets the down-going wave
+            obj.WD = WaveFromParent; % set the down-going wave for the adaptor
+            % set the waves to the 'children' according to the scattering rules
+            % 
+            a1 = WaveUp(obj.KidLeft);
+            a2 = WaveUp(obj.KidRight);
+            b1 = a2 + obj.PortRes*(a2 - a1);
+            b2 = WaveUp(obj.KidLeft) + obj.PortRes*(a1 - a2);
+       
+        end
+        function changeState(obj) % always make the state the opposite of what it was
+            if obj.state == 1
+                obj.state = 0;
+            else
+                obj.state = 1;
+            end
+        end
+        function adapt(obj)
+            if isa(obj.KidLeft, 'ser')
+                adapt(obj.KidLeft)
+            end
+            if isa(obj.KidRight, 'ser')
+                adapt(obj.KidRight)
+            end
+            if isa(obj.KidLeft, 'Parallel')
+                adapt(obj.KidLeft)
+            end
+            if isa(obj.KidRight, 'Parallel')
+                adapt(obj.KidRight)
+            end
+            
+            if isa(obj, 'ser')
+                obj.PortRes = obj.KidLeft.PortRes+obj.KidRight.PortRes;
+            end
+            if isa(obj, 'Parallel')
+                obj.PortRes = (obj.KidLeft.PortRes * obj.KidRight.PortRes)/(obj.KidLeft.PortRes + obj.KidRight.PortRes); % obj.G2+obj.G3; % parallel adapt. port facing the root
+            end
+        end
+    end
+end
