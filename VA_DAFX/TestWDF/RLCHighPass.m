6c300b5d1dccb0f5a94e4dc9e9ac0b33aa8b6366
Nikolaj Andersson <nikolajandersson@gmail.com>
2017-05-20 15:48:03 +0200

    better values for decaying RLC sinusiod

--- VA_DAFX/TestWDF/RLCHighPass.m
+++ VA_DAFX/TestWDF/RLCHighPass.m
@@ -1,10 +1,5 @@
 Fs = 44100; % sample rate (Hz)
-N = 80000; % number of samples to simulate
-
-t = 0:N-1; % time vector for the excitation
-gain = 30; % input signal gain parameter
-f0 = 20000; % excitation frequency (Hz)
-input = gain.*sin(2*pi*f0/Fs.*t); % the excitation signal
+N = 40000; % number of samples to simulate
 
 output = zeros(N,1);
 
@@ -16,7 +11,7 @@ L1 = Inductor(2*Lval*Fs);
 L1.State = 1;
 %L1.State = 1;
 %C1.State = 1;
-R1 = Resistor(80); % create the capacitance
+R1 = Resistor(2); 
 s1 =  Series(C1,Parallel(L1,R1)); % create WDF tree as a ser. conn. of V1,C1, and R1
 %r = 1/(2*pi*sqrt(CapVal*Lval)) % resonant frequency, from wiki: https://en.wikipedia.org/wiki/LC_circuit
 
