6cc26537d9e27d4f9d7c74ade052619d4b3b259f
Nikolaj Andersson <nikolajandersson@gmail.com>
2017-05-20 15:39:37 +0200

    testing RLC with parallel and series

--- VA_DAFX/TestWDF/RLCHighPass.m
+++ VA_DAFX/TestWDF/RLCHighPass.m
@@ -1,17 +1,22 @@
 Fs = 44100; % sample rate (Hz)
-N = 20000; % number of samples to simulate
+N = 80000; % number of samples to simulate
+
 t = 0:N-1; % time vector for the excitation
 gain = 30; % input signal gain parameter
 f0 = 20000; % excitation frequency (Hz)
 input = gain.*sin(2*pi*f0/Fs.*t); % the excitation signal
+
 output = zeros(N,1);
-CapVal = 1e-9; % the capacitance value in Farads
+
+CapVal = 3.5e-2; % the capacitance value in Farads
 C1 = Capacitor(1/(2*CapVal*Fs));
-Lval = 1;
+
+Lval = 3.6e-5;
 L1 = Inductor(2*Lval*Fs);
+L1.State = 1;
 %L1.State = 1;
 %C1.State = 1;
-R1 = Resistor(1000); % create the capacitance
+R1 = Resistor(80); % create the capacitance
 s1 =  Series(C1,Parallel(L1,R1)); % create WDF tree as a ser. conn. of V1,C1, and R1
 %r = 1/(2*pi*sqrt(CapVal*Lval)) % resonant frequency, from wiki: https://en.wikipedia.org/wiki/LC_circuit
 
@@ -19,14 +24,11 @@ s1 =  Series(C1,Parallel(L1,R1)); % create WDF tree as a ser. conn. of V1,C1, an
 
 
 for i=1:N
-    WaveUp(s1); % get the waves up to the root
-    setWD(s1,input(i)); % open circuit structure b = 0?
-    output(i) = Voltage(R1);
-%     if rand(1) < 0.5
-%         L1.State = L1.State + 0.1;
-%     end
+   b = WaveUp(s1); % get the waves up to the root
+    setWD(s1,-b); 
+    output(i) = getState(C1);
 end
 %% 
-plot(input);hold on; 
+
 plot(output)
 soundsc(output, Fs)
