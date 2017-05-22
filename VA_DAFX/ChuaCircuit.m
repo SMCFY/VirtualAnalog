d20c4ed334ed77dd5f71083ae3b0da2d34f27ae8
Nikolaj Andersson <nikolajandersson@gmail.com>
2017-05-22 15:06:11 +0200

    debugging Chua

--- VA_DAFX/ChuaCircuit.m
+++ VA_DAFX/ChuaCircuit.m
@@ -1,28 +1,31 @@
 clear; clc
 % C4 and L in parallel --> Series(P, R1) --> Parallel(S1, C2) --> Nonlinear Resistor
-fs = 44100;
+us = 10e-5;
+fs = 1/us;
 len = 20000; % length in samples
 
 C4val = 49e-5;
 C4 = Capacitor(1/(2*C4val*fs));
-C4.State = 20000;
+%C4.State = 20000;
 
 Lval = 7.07e-2;
 L1 = Inductor(2*Lval*fs);
-L1.State = 100000;
+L1.State = 1;
 
 P1 = Parallel(C4,L1);
 
-R1 = Resistor(1428); % 1.428kOhm
+R1 = Resistor(0.1428); % 1.428kOhm
 O1 = OpenCircuit(1428);
 S1 = Series(P1, R1);
 
 C1val = 5.5e-3;
 C1 = Capacitor(1/(2*C1val*fs));
-C1.State = 200000;
+%C1.State = 200000;
+%NLR = NLResistor(569.2);
 P2 = Parallel(S1, C1);
 
-NLR = NLResistor(569.2);
+%S1 = Series(P1, P2);
+
 
 output = zeros(len,1);
 % C1.State = 100;
@@ -30,9 +33,11 @@ output = zeros(len,1);
 for i=1:len
     a = WaveUp(P2); % get the waves up to the root
     %P2.WU
-    b = setWD(NLR, a);
+    %b = setWD(NLR, a);
     %NLR.WD
-    setWD(P2, b); 
+    
+    %b = calNLRes(a);
+    setWD(P2, a); 
     %P2.WD
     output(i) = Voltage(C4);
     output2(i) = Voltage(C1);
@@ -40,3 +45,4 @@ for i=1:len
 end
 %%
 plot(output)
+%soundsc(output,fs)
