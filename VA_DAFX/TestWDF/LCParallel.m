053002fa6095113c192ec83d26ed28c1b587b43b
Nikolaj Andersson <nikolajandersson@gmail.com>
2017-05-29 16:28:21 +0200

    added comments for LC circuits

--- VA_DAFX/TestWDF/LCParallel.m
+++ VA_DAFX/TestWDF/LCParallel.m
@@ -13,16 +13,16 @@ Lval = 3.6e-4;
 %L1 = Inductor(Fs/2*Lval)
 L1 = Inductor(Fs/2*Lval)
 
-L1.State = 100;
+L1.State = 1;
  
  
 p1 =  Parallel(C1,L1); % create WDF 
 r = 1/(2*pi*sqrt(CapVal*Lval)) % resonant frequency, from wiki: https://en.wikipedia.org/wiki/LC_circuit
-  
+
 for i=1:N
     myB = WaveUp(p1); 
-    WaveDown(p1,myB); % open circuit 
-    output(i) = getState(C1);
+    WaveDown(p1,myB); % open circuit b[n] = a[n] 
+    output(i) = Voltage(C1);
 end
 plot(output)
 %soundsc(output, Fs)
