aa01b074608c31e741d23375c2625656be6972a0
aleixcm <aleixcm@gmail.com>
2017-05-31 22:05:11 +0200

    added plot and changed parameters in LC circuits

--- VA_DAFX/TestWDF/LCParallel.m
+++ VA_DAFX/TestWDF/LCParallel.m
@@ -2,30 +2,27 @@
 % of the inductor. We are not using short or open circuit two ports,
 
 Fs = 44100; % sample rate (Hz)
-N = 20000; % number of samples to simulate
-
-Fs = 44100; % sample rate (Hz)
-N = 20000; % number of samples to simulate
- 
+N = 20000; % number of samples to simulate 
 output = zeros(N,1);
  
-CapVal = 3.6e-6; % the capacitance value in Farads
+CapVal = 3.6e-4; % the capacitance value in Farads
 C1 = Capacitor(1/(2*CapVal*Fs));
- 
-Lval = 3.6e-2;
-%L1 = Inductor(Fs/2*Lval)
-L1 = Inductor(2*Lval*Fs)
-
+Lval = 3.6e-4;
+L1 = Inductor(Fs/2*Lval);
 L1.State = 100;
- 
- 
+
 p1 =  Parallel(C1,L1); % create WDF 
+
 r = 1/(2*pi*sqrt(CapVal*Lval)) % resonant frequency, from wiki: https://en.wikipedia.org/wiki/LC_circuit
-  
+
 for i=1:N
     myB = WaveUp(p1); 
-    WaveDown(p1,myB); % open circuit 
-    output(i) = getState(C1);
+    WaveDown(p1,myB); % open circuit b[n] = a[n] 
+    output(i) = Voltage(C1);
 end
-plot(output)
+%%
+plot(output);
+xlabel('Time','FontSize', 14)
+ylabel('Voltage over C1',  'FontSize', 14)
+title('LC Parallel', 'FontSize', 18 )
 %soundsc(output, Fs)
